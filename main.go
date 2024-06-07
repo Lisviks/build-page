1237682de07138c325ebfbef601509f36a339e2d
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2024-06-07 20:23:48 +0100

    edit just to commit, and open file

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("No file provided")
+		return
+	}
+
+	file, err := os.ReadFile(args[0])
+
+	if err != nil {
+		log.Fatalf("Failed to open file: %s", err)
+	}
+
+	fmt.Println(string(file))
 	fmt.Println("Build page")
 }
