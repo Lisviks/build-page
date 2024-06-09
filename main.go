906261c3e76221cebd323bb2beec0a1266f5ad48
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2024-06-09 21:13:17 +0100

    something like this?

--- main.go
+++ main.go
@@ -4,22 +4,33 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
 	args := os.Args[1:]
 
-	if len(args) == 0 {
+	if len(args) < 2 {
 		fmt.Println("No file provided")
 		return
 	}
 
-	file, err := os.ReadFile(args[0])
+	template, err := os.ReadFile(args[0])
+	content, err := os.ReadFile(args[1])
 
 	if err != nil {
 		log.Fatalf("Failed to open file: %s", err)
 	}
 
-	fmt.Println(string(file))
+	filePath := args[1]
+	fileNameWithExt := filepath.Base(filePath)
+	ext := filepath.Ext(fileNameWithExt)
+	fileName := strings.TrimSuffix(fileNameWithExt, ext)
+
+	html := strings.Replace(string(template), "{{.Body}}", string(content), 1)
+	html = strings.Replace(html, "{{.Title}}", fileName, 1)
+
+	fmt.Println(html)
 	fmt.Println("Build page")
 }
