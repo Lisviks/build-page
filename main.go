6d4532a0751cc211a8ab73901f6a7c0841a29a5a
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2024-06-11 13:43:31 +0100

    edit

--- main.go
+++ main.go
@@ -28,8 +28,8 @@ func main() {
 	ext := filepath.Ext(fileNameWithExt)
 	fileName := strings.TrimSuffix(fileNameWithExt, ext)
 
-	html := strings.Replace(string(template), "{{.Body}}", string(content), 1)
-	html = strings.Replace(html, "{{.Title}}", fileName, 1)
+	html := strings.Replace(string(template), "{{Body}}", string(content), 1)
+	html = strings.Replace(html, "{{Title}}", fileName, 1)
 
 	err = os.MkdirAll("out", 0755)
 	if err != nil {
